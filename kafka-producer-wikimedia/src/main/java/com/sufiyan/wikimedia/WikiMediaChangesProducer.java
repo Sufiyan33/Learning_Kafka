f63da9b6a6362e6aad79eb85bf8ca0e517977bb4
Sufiyan33 <Basu700736@gmail.com>
2023-09-20 18:56:05 +0530

    Added eventHandler to trigger event

--- kafka-producer-wikimedia/src/main/java/com/sufiyan/wikimedia/WikiMediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/com/sufiyan/wikimedia/WikiMediaChangesProducer.java
@@ -1,5 +1,41 @@
 package com.sufiyan.wikimedia;
 
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
 public class WikiMediaChangesProducer {
 
+	/*
+	 * Writing producer to send data which are coming from stream to consumer.
+	 */
+	public static void main(String[] args) throws InterruptedException {
+
+		// Create producer properties.
+		Properties properties = new Properties();
+		properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+		properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+		// Create producer
+		KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+		String topic = "wikimedia.recentchange";
+
+		// Now create EventHandler to trigger event.
+		String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+
+		// EventSource.Builder builder = new EventSource.Builder(eventHandler,
+		// URI.create(url));
+//		EventSource eventsource = builder.build();
+
+		// Start the producer in another thread.
+		// eventsource.start();
+
+		// we produce for 10 minutes and block the program until then.
+		TimeUnit.MINUTES.sleep(10);
+	}
 }
