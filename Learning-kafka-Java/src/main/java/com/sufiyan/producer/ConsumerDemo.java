02c57be1c2847ca25a06d9770e6e769b0360ac5f
Sufiyan33 <Basu700736@gmail.com>
2023-10-08 17:02:22 +0530

    Added Upstash login details

--- Learning-kafka-Java/src/main/java/com/sufiyan/producer/ConsumerDemo.java
+++ Learning-kafka-Java/src/main/java/com/sufiyan/producer/ConsumerDemo.java
@@ -44,6 +44,7 @@ public class ConsumerDemo {
 		properties.setProperty("bootsrap.servers", "127.0.0.1:9092");
 
 		// Connect to Conduktor playground or Remote server.
+
 		properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
 		properties.setProperty("security.protocol", "SASL_SSL");
 		properties.setProperty("sasl.jaas.config",
@@ -55,6 +56,23 @@ public class ConsumerDemo {
 		properties.setProperty("value.deserializer", StringDeserializer.class.getName());
 		properties.setProperty("group.id", groupId);
 		properties.setProperty("auto.offset.reset", "earliest");
+
+		// Connect to Upstash playground
+
+		/*
+		 * properties.setProperty("bootstrap.servers",
+		 * "exact-guinea-8977-eu2-kafka.upstash.io:9092");
+		 * properties.setProperty("security.protocol", "SASL_SSL");
+		 * properties.setProperty("sasl.jaas.config",
+		 * "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"ZXhhY3QtZ3VpbmVhLTg5Nzck0mltKf531HjM6jcOM2CG9qpHS35Zfq8ZGV8HmUI\" password=\"MmFiOTg3ZjYtYzJkYS00NjA0LThlYmYtNTUxMTRjNzZkMzY1\";"
+		 * ); properties.setProperty("sasl.mechanism", "SCRAM-SHA-256");
+		 * 
+		 * // Set consumer config properties.setProperty("key.deserializer",
+		 * StringDeserializer.class.getName());
+		 * properties.setProperty("value.deserializer",
+		 * StringDeserializer.class.getName()); properties.setProperty("group.id",
+		 * groupId); properties.setProperty("auto.offset.reset", "earliest");
+		 */
 
 		// Create Consumer
 		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
